e125bc3ea4a83b66d2dd976a331440ae7dcfc628
Carl Olsen <unstoppablecarlolsen@gmail.com>
2019-12-18 11:10:05 -0500

    fix abilities defensive system import

--- app/Console/Commands/ImportAbilities.php
+++ app/Console/Commands/ImportAbilities.php
@@ -9,29 +9,9 @@ use League\Csv\Writer;
 
 class ImportAbilities extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'import:abilities';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    protected $description = 'Import Abilities from csv to json';
 
     public function handle()
     {
@@ -47,28 +27,27 @@ class ImportAbilities extends Command
 
             $data[] = [
 
-                'id'                   => $row['id'],
-                'name'                 => $row['name'],
-                'display_name'         => $row['display_name'],
-                'icon'                 => $row['icon'],
-                'display_order'        => $row['id'],
-                'cost_static'          => $row['cost_static'] ?: null,
-                'cost_infantry'        => $row['cost_infantry'] ?: null,
-                'cost_cavalry'         => $row['cost_cavalry'] ?: null,
-                'cost_vehicle_class_1' => $row['cost_vehicle_class_1'] ?: null,
-                'cost_vehicle_class_2' => $row['cost_vehicle_class_2'] ?: null,
-                'cost_vehicle_class_3' => $row['cost_vehicle_class_3'] ?: null,
-                'cost_vehicle_class_4' => $row['cost_vehicle_class_4'] ?: null,
-                'cost_vehicle_class_5' => $row['cost_vehicle_class_5'] ?: null,
-                'infantry_valid'       => $row['infantry_valid'] ?: null,
-                'cavalry_valid'        => $row['cavalry_valid'] ?: null,
-                'vehicle_valid'        => $row['vehicle_valid'] ?: null,
-                'warhead_cost_multiplier'        => $row['warhead_cost_multiplier'] ?: null,
+                'id'                      => $row['id'],
+                'name'                    => $row['name'],
+                'display_name'            => $row['display_name'],
+                'icon'                    => $row['icon'],
+                'display_order'           => $row['id'],
+                'cost_static'             => $row['cost_static'] ?: null,
+                'cost_infantry'           => $row['cost_infantry'] ?: null,
+                'cost_cavalry'            => $row['cost_cavalry'] ?: null,
+                'cost_vehicle_class_1'    => $row['cost_vehicle_class_1'] ?: null,
+                'cost_vehicle_class_2'    => $row['cost_vehicle_class_2'] ?: null,
+                'cost_vehicle_class_3'    => $row['cost_vehicle_class_3'] ?: null,
+                'cost_vehicle_class_4'    => $row['cost_vehicle_class_4'] ?: null,
+                'cost_vehicle_class_5'    => $row['cost_vehicle_class_5'] ?: null,
+                'infantry_valid'          => $row['infantry_valid'] ?: null,
+                'cavalry_valid'           => $row['cavalry_valid'] ?: null,
+                'vehicle_valid'           => $row['vehicle_valid'] ?: null,
+                'warhead_cost_multiplier' => $row['warhead_cost_multiplier'] ?: null,
+                'is_defensive'            => $row['is_defensive'] ?: null,
             ];
         }
 
         file_put_contents('source-data/imported/abilities.json', json_encode($data, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT));
-
     }
-
 }
