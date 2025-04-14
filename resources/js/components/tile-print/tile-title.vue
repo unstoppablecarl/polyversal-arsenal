b45381db21c3b00bb65d7a08bdbbfa0ff70261a2
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-04-14 17:00:40 -0400

    fix invert title

--- resources/js/components/tile-print/tile-title.vue
+++ resources/js/components/tile-print/tile-title.vue
@@ -1,12 +1,12 @@
 <template>
 
     <g>
-        <text :x="268.3 * 0.5" y="20" :class="['title', {'text-invert': invert}]">
+        <text :x="268.3 * 0.5" y="20" :class="['title', {'text-invert': inverted}]">
             <tspan v-for="(row, index) in tileNameArray" :x="268.3 * 0.5" :y="20 + (index) * nameLineHeight"
                    v-html="row"></tspan>
         </text>
         <text :x="268.3 * 0.5" :y="32 + ((tileNameArray.length - 1) * nameLineHeight)"
-              :class="['subtitle', {'text-invert': invert}]">{{ subtitle }}
+              :class="['subtitle', {'text-invert': inverted}]">{{ subtitle }}
         </text>
     </g>
 
@@ -25,13 +25,11 @@ export default {
     },
     data() {
         return {
-            invert: false,
             nameLineHeight: 12,
         };
     },
     computed: {
         tileNameArray() {
-            console.log(this)
             if (this.title) {
 
                 let result = [];
